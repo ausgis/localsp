bb8118f0ce02617254b2b580755e13b40c3c144c
SpatLyu <lyu.geosocial@gmail.com>
2026-02-08 01:59:57 +0800

    implement local interaction detection

--- R/globals.R
+++ R/globals.R
@@ -1,1 +1,3 @@
-utils::globalVariables(c("rid"))
+utils::globalVariables(c("rid", "pd", "sig", "variable1", "variable2", "Interaction", 
+                         "Variable1 and Variable2 interact Q-statistics", 
+                         "Variable1 and Variable2 interact P-value"))
